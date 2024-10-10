4033d1ab9ef1b25e965bebd9be11ae5ac7e079b7
MD. Saraf Uddin <39001823+Sarafuddin000@users.noreply.github.com>
2024-10-10 16:58:15 +0600

    Update Long raw to blob JPEG.sql

--- Long raw to blob JPEG.sql
+++ Long raw to blob JPEG.sql
@@ -2,40 +2,40 @@
 CREATE TABLE PMIS_OPL.PMIS_EMP_OTHERS_TMP
 (
   EMP_CODE               VARCHAR2(10 BYTE)      NOT NULL,
-  EMP_PIC_BLOB                blob,
-  SLNO                   number
+  EMP_PIC_BLOB                BLOB,
+  SLNO                   NUMBER
 );
 
 
 --Use an SLNO to limit and faster chunk by chunk process management
-alter table PMIS_OPL.PMIS_EMP_OTHERS
-add slno number;
+ALTER TABLE PMIS_OPL.PMIS_EMP_OTHERS
+ADD SLNO NUMBER;
 
 
 
 --Update SLNO increment by 1 to work like primary key
 DECLARE
-    CURSOR c1 IS SELECT emp_code, slno FROM PMIS_OPL.PMIS_EMP_OTHERS;
-sln number := 0;
+    CURSOR C1 IS SELECT EMP_CODE, SLNO FROM PMIS_OPL.PMIS_EMP_OTHERS;
+SLN NUMBER := 0;
 BEGIN
-    FOR i IN c1
+    FOR I IN C1
     LOOP
         --update a serial number for curosr query
-        sln := sln + 1;
-        update PMIS_OPL.PMIS_EMP_OTHERS
-        set slno = sln
-        where emp_code = i.emp_code;
-        commit;
-    end loop;
-end;
+        SLN := SLN + 1;
+        UPDATE PMIS_OPL.PMIS_EMP_OTHERS
+        SET SLNO = SLN
+        WHERE EMP_CODE = I.EMP_CODE;
+        COMMIT;
+    END LOOP;
+END;
 
 
 -- Convert Long RAW into blob by inserting into temp table
 DECLARE
-    CURSOR c1 IS SELECT emp_code, slno FROM PMIS_OPL.PMIS_EMP_OTHERS where slno between 20001 and 25000;
+    CURSOR C1 IS SELECT EMP_CODE, SLNO FROM PMIS_OPL.PMIS_EMP_OTHERS WHERE SLNO BETWEEN 20001 AND 25000;
     
 BEGIN
-    FOR i IN c1
+    FOR I IN C1
     LOOP
 
         INSERT INTO PMIS_OPL.PMIS_EMP_OTHERS_TMP (EMP_CODE,EMP_PIC_BLOB,SLNO)
@@ -43,7 +43,7 @@ BEGIN
                    TO_LOB(EMP_PIC),
                    SLNO
               FROM PMIS_OPL.PMIS_EMP_OTHERS
-             WHERE emp_code = i.emp_code;
+             WHERE EMP_CODE = I.EMP_CODE;
 
         COMMIT;
     END LOOP;
@@ -53,19 +53,19 @@ END;
 
 --Copy converted blob into blob column of main table
 DECLARE
-    CURSOR c2 IS
-        SELECT emp_code, EMP_PIC_BLOB
-          FROM PMIS_OPL.PMIS_EMP_OTHERS_tmp
-         WHERE slno BETWEEN 1 AND 1000;
+    CURSOR C2 IS
+        SELECT EMP_CODE, EMP_PIC_BLOB
+          FROM PMIS_OPL.PMIS_EMP_OTHERS_TMP
+         WHERE SLNO BETWEEN 1 AND 1000;
 BEGIN
-    FOR j IN c2
+    FOR J IN C2
     LOOP
         UPDATE PMIS_OPL.PMIS_EMP_OTHERS
-           SET EMP_PIC_BLOB = j.EMP_PIC_BLOB
-         WHERE emp_code = j.emp_code AND pic_ver = j.pic_ver;
+           SET EMP_PIC_BLOB = J.EMP_PIC_BLOB
+         WHERE EMP_CODE = J.EMP_CODE AND PIC_VER = J.PIC_VER;
 
         COMMIT;
-        DBMS_OUTPUT.put_line (j.emp_code);
+        DBMS_OUTPUT.PUT_LINE (J.EMP_CODE);
     END LOOP;
 END;
 
